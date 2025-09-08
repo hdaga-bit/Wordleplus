f51b2fd3ff586cba4bc1d3c029e6a5fddf5500aa
Harold Dagadu <hdagadu13@gmail.com>
2025-09-08 19:42:36 +0000

    deploymenyyy

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,27 +1,29 @@
-// // client/src/socket.js
-// import { io } from "socket.io-client";
-
-// const URL = import.meta.env.VITE_SERVER_URL;
-
-// export const socket = io(URL, {
-//   transports: ["websocket"], // more reliable on platform hosts
-//   withCredentials: false,
-// });
 // client/src/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8080", {
-  transports: ["websocket"],
-  withCredentials: false,
-  path: "/socket.io",
-});
+const URL = import.meta.env.VITE_SERVER_URL;
 
-socket.on("connect", () => {
-  console.log("[socket] connected", { id: socket.id, uri: socket.io.uri });
+export const socket = io(URL, {
+  transports: ["websocket"], // more reliable on platform hosts
+  withCredentials: false,
 });
-socket.on("connect_error", (e) => {
-  console.error("[socket] connect_error:", e?.message || e);
-});
+//client/src/socket.js
+
+//for local development
+// import { io } from "socket.io-client";
+
+// export const socket = io("http://localhost:8080", {
+//   transports: ["websocket"],
+//   withCredentials: false,
+//   path: "/socket.io",
+// });
+
+// socket.on("connect", () => {
+//   console.log("[socket] connected", { id: socket.id, uri: socket.io.uri });
+// });
+// socket.on("connect_error", (e) => {
+//   console.error("[socket] connect_error:", e?.message || e);
+// });
 
 // client/src/socket.js
 // import { io } from "socket.io-client";
